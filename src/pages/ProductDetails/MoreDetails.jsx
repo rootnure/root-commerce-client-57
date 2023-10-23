35bf3b522da1ab34c067f25dd89d0760d9729cfa
rootnure <nur.diu.2791@gmail.com>
2023-10-23 19:19:53 +0600

    More details loaded to details page

---
+++ src/pages/ProductDetails/MoreDetails.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+
+const MoreDetails = ({ id }) => {
+
+    const [details, setDetails] = useState([]);
+    useEffect(() => {
+        fetch(`https://57-root-server.vercel.app/details/${id}`)
+            .then(res => res.json())
+            .then(data => setDetails(data))
+    }, [id]);
+
+    console.log(details);
+
+    return (
+        <section>
+            <h2 className="text-4xl divider font-pacifico text-orange-600 my-12">More Details</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr className='bg-orange-600 text-white'>
+                            <th className='w-2/12'>Name</th>
+                            <th className='w-10/12'>Specification</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row */}
+                        {
+                            details.map((spec, idx) => <tr key={idx}>
+
+                                <td className='font-bold'>{spec.name}</td>
+                                <td>{spec.value}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    );
+};
+
+MoreDetails.propTypes = {
+    id: PropTypes.string.isRequired
+}
+
+export default MoreDetails;
--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -6,18 +6,17 @@ import { useEffect, useState } from "react";
 import { TbCurrencyTaka } from "react-icons/tb";
 import Rating from "../../components/Rating/Rating";
 import Swal from "sweetalert2";
+import MoreDetails from "./MoreDetails";
 
 
 const ProductDetails = () => {
 
     const user = { email: 'nur.diu.2791@gmail.com' };
-
     const { email } = user;
 
     const navigate = useNavigate();
 
     const [oldCart, setOldCart] = useState([]);
-
     useEffect(() => {
         fetch(`https://57-root-server.vercel.app/cart/${email}`)
             .then(res => res.json())
@@ -25,7 +24,6 @@ const ProductDetails = () => {
     }, [email]);
 
     const productInfo = useLoaderData();
-
     const { _id: id, img, name, brand_name, price, short_description, rating } = productInfo;
 
     const handleQuantity = isIncreased => {
@@ -71,51 +69,54 @@ const ProductDetails = () => {
                         timer: 3000,
                         timerProgressBar: true,
                         showConfirmButton: false
-                    }).then(navigate(`/products/${brand_name}`))
+                    }).then(navigate(`/${brand_name}`))
                 }
             })
     }
 
     return (
-        <section>
-            <Helmet>
-                <title>{name} | root</title>
-            </Helmet>
-            <div className="my-6">
-                <h3><Link className="text-xl font-pacifico flex items-center gap-2 p-4 hover:bg-orange-600 w-fit rounded-lg hover:text-white duration-150" to={`/${brand_name}`}><BsArrowLeft></BsArrowLeft>Back to {brand_name}</Link></h3>
-            </div>
-            <div className="grid grid-cols-3 gap-6">
-                <div className="card card-compact">
-                    <figure className="h-[350px] p-4">
-                        <img src={img} alt={name} className="max-h-full" />
-                    </figure>
+        <>
+            <section>
+                <Helmet>
+                    <title>{name} | root</title>
+                </Helmet>
+                <div className="my-6">
+                    <h3><Link className="text-xl font-pacifico flex items-center gap-2 p-4 hover:bg-orange-600 w-fit rounded-lg hover:text-white duration-150" to={`/${brand_name}`}><BsArrowLeft></BsArrowLeft>Back to {brand_name}</Link></h3>
                 </div>
-                <div className="col-span-2 space-y-4">
-                    <h2 className="text-3xl text-orange-600 font-exo-2 font-semibold">{name}</h2>
-                    <div className="flex gap-4">
-                        <p className="px-2 py-0.5 rounded-full bg-orange-300 flex gap-1"><span>Price: </span><span className="font-semibold flex items-center">{price}<TbCurrencyTaka></TbCurrencyTaka></span></p>
-                        <p className="px-2 py-0.5 rounded-full bg-orange-300 flex gap-1"><span>Status: </span><span className="font-semibold">In Stock</span></p>
-                        <p className="px-2 py-0.5 rounded-full bg-orange-300 flex gap-1"><span>Brand: </span><span className="font-semibold">{brand_name}</span></p>
+                <div className="grid grid-cols-3 gap-6">
+                    <div className="card card-compact">
+                        <figure className="h-[350px] p-4">
+                            <img src={img} alt={name} className="max-h-full" />
+                        </figure>
                     </div>
-                    <div>
-                        <h3 className="text-xl font-semibold">Key Features</h3>
-                        <p className="mt-2 leading-7">{short_description}</p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <span>Rating:</span>
-                        <Rating rating={rating} />
-                    </div>
-                    <form onSubmit={handleAddToCart} className="flex gap-x-8">
-                        <div className="w-fit flex items-center border-2">
-                            <p onClick={() => handleQuantity(false)} className="text-lg font-bold py-1 cursor-pointer w-8 text-center h-full flex items-center justify-center hover:bg-orange-300 duration-150"><AiOutlineMinus></AiOutlineMinus></p>
-                            <input type="text" name="quantity" id="quantity" defaultValue="1" className="w-12 text-center border-x-2 py-1.5" />
-                            <p onClick={() => handleQuantity(true)} className="text-lg font-bold py-1 cursor-pointer w-8 text-center h-full flex items-center justify-center hover:bg-orange-300 duration-150"><BsPlusLg></BsPlusLg></p>
+                    <div className="col-span-2 space-y-4">
+                        <h2 className="text-3xl text-orange-600 font-exo-2 font-semibold mb-6">{name}</h2>
+                        <div className="flex gap-x-2.5">
+                            <p className="px-3 py-0.5 rounded-full bg-orange-200 flex gap-1"><span>Price: </span><span className="font-semibold flex items-center">{price}<TbCurrencyTaka></TbCurrencyTaka></span></p>
+                            <p className="px-3 py-0.5 rounded-full bg-orange-200 flex gap-1"><span>Status: </span><span className="font-semibold">In Stock</span></p>
+                            <p className="px-3 py-0.5 rounded-full bg-orange-200 flex gap-1"><span>Brand: </span><span className="font-semibold">{brand_name}</span></p>
                         </div>
-                        <button type="submit" className="px-4 py-1 border border-orange-600 font-semibold rounded text-white bg-orange-600 hover:text-orange-600 hover:bg-transparent duration-150">Add To Cart</button>
-                    </form>
+                        <div>
+                            <h3 className="text-xl font-semibold">Key Features</h3>
+                            <p className="mt-2 leading-7">{short_description}</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <span>Rating:</span>
+                            <Rating rating={rating} />
+                        </div>
+                        <form onSubmit={handleAddToCart} className="flex gap-x-8">
+                            <div className="w-fit flex items-center border-2">
+                                <p onClick={() => handleQuantity(false)} className="text-lg font-bold py-1 cursor-pointer w-8 text-center h-full flex items-center justify-center hover:bg-orange-300 duration-150"><AiOutlineMinus></AiOutlineMinus></p>
+                                <input type="text" name="quantity" id="quantity" defaultValue="1" className="w-12 text-center border-x-2 py-1.5" />
+                                <p onClick={() => handleQuantity(true)} className="text-lg font-bold py-1 cursor-pointer w-8 text-center h-full flex items-center justify-center hover:bg-orange-300 duration-150"><BsPlusLg></BsPlusLg></p>
+                            </div>
+                            <button type="submit" className="px-4 py-1 border border-orange-600 font-semibold rounded text-white bg-orange-600 hover:text-orange-600 hover:bg-transparent duration-150">Add To Cart</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <MoreDetails id={id} />
+        </>
     );
 };
 
