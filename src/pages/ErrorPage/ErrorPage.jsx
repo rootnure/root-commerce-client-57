43bba7d30aa42fac3832c1d1cc215301e8dd7fb5
rootnure <nur.diu.2791@gmail.com>
2023-10-24 23:01:34 +0600

    Error Page redesigned

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -7,8 +7,11 @@ const ErrorPage = () => {
     return (
         <>
             <Navbar />
-            <h2>Oops!!! something went wrong...</h2>
-            <Link to="/">Home</Link>
+            <main className="min-h-[600px] flex flex-col justify-center items-center">
+                <img src="https://i.ibb.co/bsv2B8z/error.gif" alt="Error GIF" />
+                <h3 className="text-orange-600 text-2xl font-pacifico my-6">Oops!!! Something went wrong...</h3>
+                <Link to="/" className="mt-4 px-2 py-1 border border-orange-600 rounded-md cursor-pointer font-bold duration-200 bg-orange-600 text-white hover:bg-white hover:text-orange-600">Back to Home</Link>
+            </main>
             <Footer />
         </>
     );
